6baaa96de12f3f96d7b78cd6ca25fbbe33e5417a
Bartosz Kozecki <bartosz.kozecki@bitpeak.pl>
2026-05-09 16:07:43 +0200

    chore: setup vitest with jsdom and testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+import '@testing-library/jest-dom/vitest'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+  },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
